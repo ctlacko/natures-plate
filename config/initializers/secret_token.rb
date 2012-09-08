55a37c576d1f240e9559fc8171e0c2f734c592b2
adminctl <adminctl@adminctl-VirtualBox.(none)>
2012-09-08 11:59:18 -0500

    Initial refinerycms project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NaturesPlate::Application.config.secret_token = '3304817c6cb47bccd5895d3ee040cca59fcad79acff3a9545dea5921703f5a8ef1d734eb70f7ebad83d785e7bf74b613f8d9a5dcf3418fffb64678fa26adc5ad'
